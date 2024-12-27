787c0f1afb429f958fbb64877c0986f4a8edf8dd
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-26 21:55:51 -0300

    chore: user entity

---
+++ src/main/java/com/aurum/vmmanager/infrastructure/entities/UserEntity.java
@@ -0,0 +1,31 @@
+package com.aurum.vmmanager.infrastructure.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@Table(name = "tb_users")
+@Entity
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String cpf;
+    private String password;
+
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+}
